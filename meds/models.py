5f993bebf0094f2c8e20a9d0d0abe79688a19042
trena <shamlima.trena@northsouth.edu>
2025-03-24 09:06:30 +0600

    update med model to support duplicate delete

--- meds/models.py
+++ meds/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
+import os
 
 class Med(models.Model):
     name = models.CharField(max_length=255)
@@ -11,3 +14,26 @@ class Med(models.Model):
 
     def __str__(self):
         return self.name
+
+# Delete the image file when the model instance is deleted
+@receiver(post_delete, sender=Med)
+def delete_image_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+# Delete the old image file when the image is updated
+@receiver(pre_save, sender=Med)
+def delete_image_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+    
+    try:
+        old_image = Med.objects.get(pk=instance.pk).image
+    except Med.DoesNotExist:
+        return False
+    
+    new_image = instance.image
+    if old_image and old_image != new_image:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
